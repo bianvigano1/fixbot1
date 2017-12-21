25dda75afcef9a47d70b3338333c5960d0df7883
bianvigano1 <34676028+bianvigano1@users.noreply.github.com>
2017-12-21 11:03:04 +0700

    Update bot.js

--- bot.js
+++ bot.js
@@ -5,6 +5,17 @@ client.on('ready', () => {
     console.log('I am ready!');
 });
 
+var prefix = ">."
+
+client.on('message', message => {
+    if (message.author.bot) return;
+    if (!message.content.startsWith(prefix)) return;
+
+    if (message.content.startsWith(prefix + 'ping')) {
+        message.channel.sendMessage('Pong! Your ping is `' + `${Date.now() - message.createdTimestamp}` + ' ms`');
+    }
+});
+
 client.on('message', message => {
     if (message.content === 'ping') {
     	message.reply('pong');
